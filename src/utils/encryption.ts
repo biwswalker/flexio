4840f25d37f5efb2e405d0dbf28cd43fc2f2408f
Biws <jennarong.sae@gmail.com>
2025-03-19 02:07:53 +0700

    Refactor JWT encryption to use AES_SECRET_KEY and update sign-in view layout

--- src/utils/encryption.ts
+++ src/utils/encryption.ts
@@ -1,12 +1,12 @@
 import CryptoJS from 'crypto-js';
 
 export function encryption(encryptext: string) {
-  const text = CryptoJS.AES.encrypt(encryptext, process.env.NEXT_PUBLIC_JWT_SECRET_KEY).toString();
+  const text = CryptoJS.AES.encrypt(encryptext, process.env.NEXT_PUBLIC_AES_SECRET_KEY).toString();
   return text || '';
 }
 
 export function decryption(decryptext: string) {
-  const text = CryptoJS.AES.decrypt(decryptext, process.env.NEXT_PUBLIC_JWT_SECRET_KEY).toString(
+  const text = CryptoJS.AES.decrypt(decryptext, process.env.NEXT_PUBLIC_AES_SECRET_KEY).toString(
     CryptoJS.enc.Utf8
   );
   return text || '';
